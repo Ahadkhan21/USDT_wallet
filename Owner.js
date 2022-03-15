29bd306ba84828db493caf49396ce6c3a1f6a560
Ahadkhan21 <87846249+Ahadkhan21@users.noreply.github.com>
2022-03-15 16:39:29 +0530

    Update Owner.js

--- Owner.js
+++ Owner.js
@@ -12,7 +12,7 @@ function Owner() {
 
     const GetBalance=(e)=> {
         e.preventDefault();
-        contract_Token.methods.balanceOf("0x765DBf076Ba1B30088353A2227922Fe06849BcA0").call().then(function(bal){
+        contract_Token.methods.balanceOf(" ").call().then(function(bal){
             console.log(bal);
             setBalance(bal/10**18);
         })   
@@ -21,18 +21,15 @@ function Owner() {
 
     const transactionHistory = async() =>{
         contract_Token.events.Transfer({
-            filter: {myIndexedParam: '0x765DBf076Ba1B30088353A2227922Fe06849BcA0'}, // Using an array means OR: e.g. 20 or 23
+            filter: {myIndexedParam: ' '}, // Enter Parameters
             fromBlock: 0
         }, function(error){  })
         .on("connected", function(subscriptionId){
             console.log(subscriptionId);
         })
         .on('data', (event)=>{
-            if (event.returnValues.to === '0x765DBf076Ba1B30088353A2227922Fe06849BcA0'){
             setEvents((events) => [...events,{blockNumber:event.blockNumber, 
-                returnValues:event.returnValues,type:"DEBIT"}])}  
-            else{setEvents((events) => [...events,{blockNumber:event.blockNumber, 
-                returnValues:event.returnValues, type:"CREDIT"}])}           
+                returnValues:event.returnValues]     
         })
         .on('error', function(error, receipt) { // If the transaction was rejected by the network with a receipt, the second parameter will be the receipt.
             console.log(error)
@@ -40,15 +37,9 @@ function Owner() {
     };
 
     const web3 = new Web3(window.ethereum);
-    const token_Add= "0x02Ae61BEaeeB0E67a8493Ff808153586D4705DD1";
+    const token_Add= " "; //Enter token contract address
     const contract_Token =  new web3.eth.Contract(token_abi, token_Add);
-    // console.log(contract_Token)
-
-    // const wallet_Add= "0x24154EE6e0FE4586aaA5A3D44c4901B6cCCefb53";    
-    // const contract_Wallet =  new web3.eth.Contract(wallet_abi, wallet_Add);
-    // console.log(contract_Wallet)
-
-    
+    // console.log(contract_Token)   
 
 
     return(
